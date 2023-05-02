51f2ada0dea0f3a6ca58b79c2719b1dfa6c34a92
Pamela Pupiales <pamelapupiales27@gmail.com>
2023-05-02 11:31:19 -0500

    Revert "Update RedMesh.py"
    This reverts commit e34525f5e333b7dfc9ebfc285a206d8f08799b0c.

--- RedMesh.py
+++ RedMesh.py
@@ -1,48 +1,122 @@
-import socket
-import threading
+import pywifi
+from pywifi import const
+import time
+import random
 
 class Nodo:
     def __init__(self, direccion_ip):
         self.direccion_ip = direccion_ip
         self.vecinos = []
-        self.socket_servidor = None
 
-    def iniciar_hilo(self):
-        self.socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket_servidor.bind((self.direccion_ip, 8000))
-        self.socket_servidor.listen()
-        thread = threading.Thread(target=self.escuchar_conexiones_entrantes)
-        thread.start()
+class RedMesh:
+    def __init__(self):
+        self.nodos = []
 
-    def escuchar_conexiones_entrantes(self):
-        while True:
-            conn, addr = self.socket_servidor.accept()
-            print(f"Conexión entrante de {addr}")
-            thread = threading.Thread(target=self.escuchar_mensajes, args=(conn,))
-            thread.start()
+    def agregar_nodo(self, direccion_ip):
+        self.nodos.append(Nodo(direccion_ip))
 
-    def escuchar_mensajes(self, conn):
-        while True:
-            data = conn.recv(1024)
-            if not data:
+    def eliminar_nodo(self, direccion_ip):
+        for nodo in self.nodos:
+            if nodo.direccion_ip == direccion_ip:
+                self.nodos.remove(nodo)
+                for vecino in nodo.vecinos:
+                    vecino.vecinos.remove(nodo)
                 break
-            print(f"Mensaje recibido en {self.direccion_ip}: {data.decode()}")
-            for vecino in self.vecinos:
-                self.enviar_mensaje(vecino, data)
-        conn.close()
 
-    def enviar_mensaje(self, direccion_ip_destino, mensaje):
-        socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket_cliente.connect((direccion_ip_destino, 8000))
-        socket_cliente.send(mensaje.encode())
-        socket_cliente.close()
+    def enrutamiento_congestion(self, direccion_ip_origen, direccion_ip_destino, datos):
+        nodo_actual = None
+        for nodo in self.nodos:
+            if nodo.direccion_ip == direccion_ip_origen:
+                nodo_actual = nodo
+                break
 
-nodo1 = Nodo("192.168.0.1")
-nodo2 = Nodo("192.168.0.2")
+        vecinos_disponibles = [vecino for vecino in nodo_actual.vecinos if len(vecino.vecinos) > 1]
 
-nodo1.vecinos.append(nodo2.direccion_ip)
-nodo2.vecinos.append(nodo1.direccion_ip)
+        if not vecinos_disponibles:
+            print("No hay vecinos disponibles para enrutar el paquete")
+            return
 
-nodo1.iniciar_hilo()
-nodo2.iniciar_hilo()
+        vecino_seleccionado = random.choice(vecinos_disponibles)
 
+        print(f"Enviando datos a {direccion_ip_destino} a través del vecino {vecino_seleccionado.direccion_ip}")
+
+    def configurar_red_inalambrica(self, ssid, password):
+        # Crear un objeto PyWiFi
+        wifi = pywifi.PyWiFi()
+        # Obtener la primera interfaz de red inalámbrica disponible
+        iface = wifi.interfaces()[0]
+        # Apagar la interfaz de red inalámbrica
+        iface.disconnect()
+        # Crear un objeto Perfil de red
+        profile = pywifi.Profile()
+        # Establecer el SSID de la red
+        profile.ssid = ssid
+        # Establecer el tipo de cifrado
+        profile.auth = const.AUTH_ALG_OPEN
+        profile.akm.append(const.AKM_TYPE_WPA2PSK)
+        # Establecer la contraseña de la red
+        profile.cipher = const.CIPHER_TYPE_CCMP
+        profile.key = password
+        # Agregar el perfil a la lista de perfiles de la interfaz de red inalámbrica
+        iface.remove_all_network_profiles()
+        temp_profile = iface.add_network_profile(profile)
+        # Conectar a la red
+        iface.connect(temp_profile)
+        # Esperar a que la conexión se establezca
+        while iface.status() != const.IFACE_CONNECTED:
+            pass
+
+        # Imprimir información de la conexión
+        print("Conectado a:", iface.ssid())
+        print("Dirección IP:", iface.iface_addr())
+
+    def iniciar(self):
+        # Configurar la red inalámbrica
+        self.configurar_red_inalambrica("MiRed", 1234)
+
+        # Configurar los nodos de la red mesh
+        for i in range(self.num_nodos):
+            nodo = Nodo(i, self)
+            self.nodos.append(nodo)
+
+        # Iniciar los hilos de los nodos
+        for nodo in self.nodos:
+            nodo.iniciar_hilo()
+
+        # Esperar a que todos los nodos se conecten
+        time.sleep(5)
+        # Establecer rutas
+        self.establecer_rutas()
+        # Iniciar la simulación
+        self.simular()
+
+
+    def enrutamiento(self, direccion_ip_origen, direccion_ip_destino, datos):
+        nodo_actual = None
+        for nodo in self.nodos:
+            if nodo.direccion_ip == direccion_ip_origen:
+                nodo_actual = nodo
+                break
+
+        for vecino in nodo_actual.vecinos:
+            if vecino.direccion_ip == direccion_ip_destino:
+                print(f"Enviando datos a {direccion_ip_destino} a través del vecino {vecino.direccion_ip}")
+                return
+
+        print(f"No se encontró un vecino directo para la dirección {direccion_ip_destino}, enviando a todos los vecinos conocidos")
+        for vecino in nodo_actual.vecinos:
+            print(f"Enviando datos a {direccion_ip_destino} a través del vecino {vecino.direccion_ip}")
+
+red = RedMesh()
+red.agregar_nodo("192.168.0.1")
+red.agregar_nodo("192.168.0.2")
+red.agregar_nodo("192.168.0.3")
+
+#red.eliminar_nodo("192.168.0.3")
+
+red.nodos[0].vecinos = [red.nodos[1], red.nodos[2]]
+red.nodos[1].vecinos = [red.nodos[0], red.nodos[2]]
+red.nodos[2].vecinos = [red.nodos[0], red.nodos[1]]
+
+red.enrutamiento("192.168.0.1", "192.168.0.2", "Hola, nodo 2")
+red.enrutamiento("192.168.0.1", "192.168.0.3", "Hola, nodo 3")
