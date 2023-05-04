6500c0eef9e5b00387d8b9e3dbca366fade55e62
Andres Martinez <solm2508@gmail.com>
2023-05-04 01:56:37 -0500

    Added: nodes ID, Show funct
    Show function to show all nodes connected to this one, also handle error lns 33 - 35 if node.recv() returned empty
    Nodes ID using uuid

--- py2p_mesh.py
+++ py2p_mesh.py
@@ -1,10 +1,11 @@
 import py2p
 import threading
 import time
+import uuid
 
-def handle_message(msg):
+def handle_message(msg, sender_id):
     clean_msg = extract_message(msg)
-    print(f"Received message: {clean_msg}")
+    print(f"Received message from {sender_id}: {clean_msg}")
 
 def extract_message(input_string):
     start_index = input_string.index("(b'") + 3
@@ -12,12 +13,18 @@ def extract_message(input_string):
     message = input_string[start_index:end_index]
     return message
 
+def show_connected_devices(node):
+    print("Connected devices:")
+    for peer in node.routing_table:
+        print(f"ID: {peer.id}, Address: {peer.addr}")
+
 def main():
     # Create a new mesh node with a specified port
     node = py2p.MeshSocket('0.0.0.0', 5678)
     
     node.timeout = 10
     node.max_connections = 5
+    print(f"This node's ID: {node.id}")
 
     # Connect to the bootstrap node if you know its address
     bootstrap_address = input("Enter the bootstrap node's address (IP:Port) or leave empty: ")
@@ -25,15 +32,15 @@ def main():
         ip, port = bootstrap_address.split(':')
         node.connect(ip, int(port))
 
-    print(f"This node's address: {node.out_addr}")
-
     # Start a thread to handle incoming messages
     def message_handler():
         while True:
-            msg, sender = node.recv()
-            if msg:
-                handle_message(msg)
+            received = node.recv()
+            if received:
+                msg, sender = received
+                handle_message(msg, sender.id)
             time.sleep(0.1)
+
 
     message_thread = threading.Thread(target=message_handler)
     message_thread.daemon = True
@@ -41,13 +48,15 @@ def main():
 
     # Send messages to the other nodes
     while True:
-        message = input("Enter message to send, 'exit' to quit, or 'disconnect' to disconnect: ")
+        message = input("Enter message to send, 'exit' or 'show': ")
 
         if message == 'exit':
             break
         elif message == 'disconnect':
             node.disconnect()
             break
+        elif message == 'show':
+            show_connected_devices(node)
         else:
             node.send(bytes(message, 'utf-8'))
 
